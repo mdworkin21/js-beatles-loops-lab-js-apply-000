b4eae56ffea5fa21cc40dbd8172b1f5d52265257
Matthew Dworkin <mdworkin21@gmail.com>
2018-02-24 19:41:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(arr1, arr2) {
   let whatTheyPlay = []
   let str = ''
 
-  for (let i = 0; i < arr1.length; i++){
+  for (let i = 0; i < arr1.length; i++) {
     str += arr1[i] + '  plays ' + arr2[i]
     whatTheyPlay.push(str);
   }
